2db2863f3cac87d3dbe0123a4a6aab8cefe300aa
Sam Ellard <sellard@TMD004.local>
2024-05-22 14:13:33 +0900

    Redoing Cloudflare Worker setup

--- src/app.d.ts
+++ src/app.d.ts
@@ -2,11 +2,21 @@
 // for information about these interfaces
 declare global {
 	namespace App {
-		// interface Error {}
-		// interface Locals {}
-		// interface PageData {}
-		// interface PageState {}
-		// interface Platform {}
+		interface Locals {}
+		interface Platform {
+			env: {
+				COUNTER: DurableObjectNamespace;
+				export async function post(context) {
+					const counter = context.platform.env.COUNTER.idFromName("A");
+				  }
+			};
+			context: {
+				waitUntil(promise: Promise<any>): void;
+			};
+			caches: CacheStorage & { default: Cache }
+		}
+		interface Session {}
+		interface Stuff {}
 	}
 }
 
